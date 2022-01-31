1008a37c316fdc34359b587603a3568932de73fd
stacyche <xc172@georgetown.edu>
2022-01-31 17:22:17 -0500

    Update game.py

--- game.py
+++ game.py
@@ -4,4 +4,16 @@
 #
 # todo: write some Python code here to satisfy the exercise objectives
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
-#
+
+
+#ASK FOR USER INPUT
+
+
+
+#VALIDATIONS
+
+#COMPUTER CHOICE
+
+#DETERMINE THE WINNER 
+
+#FINAL RESULTS 
